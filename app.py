a3ef5cb37b223d1763303b82134a5082847062a2
rodriguesfas <franciscosouzaacer@gmail.com>
2023-09-02 21:16:27 -0300

    baseline open door

--- app.py
+++ app.py
@@ -1,34 +1,4 @@
-import socketio
-from fastapi import FastAPI
-
-app = FastAPI()
-
-server_url = "http://127.0.0.1:9000"
-
-uuid = "fee13069-db12-49f3-bd47-405048867301"
-
-sio = socketio.AsyncClient()
-
-
-@app.on_event("startup")
-async def startup_event():
-    await sio.connect(f"{server_url}?uuid={uuid}")
-    print("🔌 Connected")
-
-
-@app.get("/send_message")
-async def send_message():
-    await sio.emit("receive", "Olá, servidor principal!")
-
-    return {"message": "Dados enviados com sucesso"}
-
-
-@sio.on("receive")
-async def receive(message):
-    try:
-        print(f"message: {message}")
-    except Exception as e:
-        print(f"Error: {e}")
+from app import app
 
 
 if __name__ == "__main__":
---
+++ app/__init__.py
@@ -0,0 +1,37 @@
+import socketio
+from fastapi import FastAPI
+from app.utils import get_info_raspbarrypi
+
+app = FastAPI()
+
+server_url = "http://127.0.0.1:9000"
+
+uuid = "fee13069-db12-49f3-bd47-405048867301"
+
+sio = socketio.AsyncClient()
+
+
+@app.on_event("startup")
+async def startup_event():
+    await sio.connect(f"{server_url}?uuid={uuid}")
+    print("🔌 Connected")
+
+
+@app.get("/send_message")
+async def send_message(message):
+    await sio.emit("receive_message", "Olá, servidor principal!")
+    return {"message": "Dados enviados com sucesso"}
+
+
+@sio.on("receive_message")
+async def receive_message(message):
+    try:
+        if message == "get_info_raspbarrypi":
+            info = get_info_raspbarrypi()
+            await sio.emit("receive_message", info)
+        elif message == "open_the_door":
+            print("Open door")
+            message = "Open door"
+            await sio.emit("receive_message", message)
+    except Exception as e:
+        print(f"Error: {e}")
